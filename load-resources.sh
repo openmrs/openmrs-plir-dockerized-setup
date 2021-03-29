417045e7e2ae6109fb875fc9b4407fe49df25b66
Mutesasira Moses <mozzymutesa@gmail.com>
2021-03-29 18:06:55 +0300

    Update load-resources.sh

--- load-resources.sh
+++ load-resources.sh
@@ -1,4 +1,4 @@
-curl -i -u hapi:hapi123 -H "Content-type: application/json" -X PUT http://localhost:8090/fhir/Library/library-FHIRHelpers-4.0.1 -d @./resources/fhir_helper_library.json
+curl -i -u hapi:hapi123 -H "Content-type: application/json" -X PUT http://localhost:8090/fhir/Library/library-FHIRHelpers-4.0.1 -d @./resources/fhir_helper-library.json
 
 curl -i -u hapi:hapi123 -H "Content-type: application/json" -X PUT http://localhost:8090/fhir/Library/TX-PVLS -d @./resources/tx_pvls-library.json
 
