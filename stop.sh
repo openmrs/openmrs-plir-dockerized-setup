c6da5fb7db19f76d05f61f6f500acc3ad812df5d
mozzy11 <mozzymutesa@gmail.com>
2021-04-04 08:47:05 +0300

    make scripts executable

--- stop.sh
+++ stop.sh
@@ -3,4 +3,6 @@ docker-compose down  --remove-orphans
 
 docker-compose -f pipeline-compose.yml down  --remove-orphans
 
+docker-compose -f plir-widget/docker-compose.yml down --remove-orphans
+
 docker system prune --volumes
